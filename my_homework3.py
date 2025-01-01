8b0859f95d843b4af5595b96fe6a261e834339c9
alexandervorobjev <chichavorobjev@gmail.com>
2025-01-01 19:06:07 +0300

    fdd

--- my_homework3.py
+++ my_homework3.py
@@ -29,3 +29,32 @@ def sum_numbers(list):
 list = [1,4,6,8,9]
 sum = sum_numbers(list)
 print(f'sum of list is {sum}')
+
+
+
+
+def list(obj):
+    print(obj.popitem())
+    print(obj)
+
+
+
+
+
+
+array = [2,"ooo",4,5,9,"fab",99,3]
+
+def remove_strs(list):
+    new_array = []
+
+    for value in list:
+        try:
+            int(value)
+            new_array.append(value)
+        except:
+            pass
+    
+    return new_array
+
+print(array)
+print(remove_strs(array))
